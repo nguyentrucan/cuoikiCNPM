feb326afeb25aa659e49717d101229fccde99a65
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2023-05-26 02:05:22 +0700

    first commit

---
+++ controller/productCtrl.js
@@ -0,0 +1,36 @@
+const Product = require('../models/productModel');
+const asyncHandler = require("express-async-handler");
+
+//Create a Product
+const createProduct = asyncHandler(async(req,res)=>{
+    try {
+        const newProduct = await Product.create(req.body);
+        res.json(newProduct);
+        res.json({
+        message:"Hey it's product post route",
+        }); 
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const getaProduct = asyncHandler(async(req,res)=>{
+    const {id} = req.params;
+    try {
+        const findProduct = await Product.findById(id);
+        res.json(findProduct);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const getAllProduct = asyncHandler(async(req,res)=>{
+    try {
+        const gettallProduct = await Product.find();
+        res.json(gettallProduct);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports={createProduct,getaProduct,getAllProduct};
